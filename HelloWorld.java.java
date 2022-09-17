5d24977ae58edd19b545cc4f4c3f11ca6c76e0db
akhilm29 <90561501+akhilm29@users.noreply.github.com>
2022-09-17 17:26:44 +0530

    Update HelloWorld.java.java

--- HelloWorld.java.java
+++ HelloWorld.java.java
@@ -2,20 +2,8 @@ public class HelloWorld {
 
 	public static void main (String[] args) {
 
-	 	  System.out.println("Made some changes");
+	 	  System.out.println("-----***** Made some changes *****-----");
 
 	 }
 
-}
-Public class Hello {
-    int a;
-    static int b;
-
-
-    public static void main(String[] args) {
-
-        Hello h1 = new Hello();
-        System.out.println(b);
-
-    }
 }
