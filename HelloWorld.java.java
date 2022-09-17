5dd49b5c0c8a675ad6047e87a1f3dd0046bd1cbb
Akhil Mahajan <mahajan_akhil@ymail.com>
2022-09-17 17:11:55 +0530

    Adding new files

---
+++ HelloWorld.java.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+
+	public static void main (String[] args) {
+
+	 	  System.out.println("Hello World");
+
+	 }
+
+}
+Public class Hello {
+    int a;
+    static int b;
+
+
+    public static void main(String[] args) {
+
+        Hello h1 = new Hello();
+        System.out.println(b);
+
+    }
+}
