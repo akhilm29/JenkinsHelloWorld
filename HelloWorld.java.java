dc1e21f2e471ae6b6596006f94abe7fd5a3f575b
akhilm29 <90561501+akhilm29@users.noreply.github.com>
2023-04-07 03:06:15 +0000

    Update HelloWorld.java.java

--- HelloWorld.java.java
+++ HelloWorld.java.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 	public static void main (String[] args) {
 
-	 	  System.out.println("-----***** Tyring Webhooks *****-----");
+	 	  System.out.println("----***** Tyring Webhooks *****----");
 
 	 }
 
